1f3ac8041edfc88a6d97e4ac8dfe5b18457a7c0d
Victor Yanez <victoryanez11@gmail.com>
2023-02-12 18:22:29 -0600

    Initial commit

---
+++ MManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MManejoPresupuesto.Models;
+
+namespace MManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly string connectionString;
+
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public IActionResult Crear()
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+
+            {
+
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
