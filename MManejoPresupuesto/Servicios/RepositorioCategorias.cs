c2c57614be35a3a826305eac6bb018a63dab029d
VictorYanez <victoryanez11@gmail.com>
2025-11-28 15:58:27 -0600

    Definición de la Entidad Categoría la enumeración TipoOperacion y el repositorio.
    Definición del modelo Categoría con atributos de validación, la enumeración TipoOperacion para los tipos de operación y el servicio RepositorioCategorias con un método para crear categorías en la base de datos. Estas adiciones admiten la funcionalidad de gestión de categorías en la aplicación.

---
+++ MManejoPresupuesto/Servicios/RepositorioCategorias.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using MManejoPresupuesto.Models;
+
+namespace MManejoPresupuesto.Servicios
+{
+
+    public interface IRepositorioCategorias
+        {
+    }
+    public class RepositorioCategorias : IRepositorioCategorias
+    {
+        private readonly string connectionString;
+        public RepositorioCategorias(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Categoria categoria)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"
+                                        INSERT INTO Categorias (Nombre, TipoOperacionId, UsuarioId)
+                                        Values (@Nombre, @TipoOperacionId, @UsuarioId);
+
+                                        SELECT SCOPE_IDENTITY();
+                                        ", categoria);
+
+            categoria.Id = id;
+        }
+
+
+
+    }
+
+
+}
